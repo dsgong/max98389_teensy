da36678fb5760a49f689aa41ce86572684f86c21
David Gong <david.s.gong@gmail.com>
2025-02-18 14:02:16 -0600

    Simplified Main

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,11 @@
 
 
 // GUItool: begin automatically generated code
-AudioSynthWaveform       waveform1;      //xy=188,240
-AudioEffectEnvelope      envelope1;      //xy=371,237
-
 AudioInputI2SQuad        i2s_quad1;      //xy=301,598
 AudioOutputI2S           i2s2;           //xy=565,241
 AudioInputUSB            usb1;           //xy=157,294
 AudioOutputUSB           usb2;           //xy=405,293
 
-AudioConnection          patchCord1(waveform1, envelope1);
 AudioConnection          patchCord2(usb1, 0, i2s2, 0);
 AudioConnection          patchCord3(usb1, 1, i2s2, 1);
 AudioConnection          patchCord4(i2s_quad1, 2, usb2, 0);
@@ -21,8 +17,6 @@ AudioConnection          patchCord5(i2s_quad1, 3, usb2, 1);
 bool configured = false;
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-
     // Enable the serial port for debugging
     Serial.begin(9600);
     Serial.println("Started");
@@ -36,31 +30,9 @@ void setup() {
     } else {
         Serial.println("Not configured");
     }
-
     AudioMemory(128);
-
-    //waveform1.pulseWidth(0.5);
-    waveform1.begin(0.1, 306, WAVEFORM_SINE);
-
-    envelope1.attack(0);
-    envelope1.decay(0);
-    envelope1.release(0);
-
-    envelope1.noteOn();
 }
 
 void loop() {
 
-    float w;
-    for (uint32_t i =1; i<20; i++) {
-        w = i / 20.0;
-        //waveform1.pulseWidth(w);
-        
-        /*digitalWrite(LED_BUILTIN, HIGH);
-        delay(800);
-        envelope1.noteOff();
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(600);
-        */
-    }
 }
